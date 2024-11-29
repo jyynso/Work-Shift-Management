41444e4fed7dcfee63a20eb51d11d5e48b8ce917
jynso <ecwt26@gmail.com>
2024-11-29 23:14:00 +0800

    working on the dashboards

---
+++ app/dashboardUser/page.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+
+const dashboardUser = () => {
+  return (
+    <div className='container'>
+
+      <div className='formContainer'>
+        <h1 className='textOnTop' 
+        style={{top:'20px',
+               left:'40px',
+               position:'absolute'}}>Hello there, Pedro Pedro</h1>
+      </div>
+      <section>
+      <div className='box'
+      style={{top:'20px',
+              left:'20px'
+      }}>
+     
+      </div>
+      </section>
+    </div>
+  );
+};
+
+export default dashboardUser;
